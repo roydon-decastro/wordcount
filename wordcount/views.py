2bb2552d6f45b66638462d1f81c4cd4d379c8228
Roydon <roydon.decastro@gmail.com>
2018-10-07 21:49:12 +0800

    This is my first commit

---
+++ wordcount/views.py
@@ -0,0 +1,12 @@
+from django.http import  HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request,'home.html')
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    wordlist = fulltext.split()
+
+    return render(request,'count.html', {'fulltext':fulltext, 'wordcount': len(wordlist)})
